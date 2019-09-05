27aea9ce53f907176e648d8e19819719afb3ade9
Liam Adams <lbadams2@ncsu.edu>
2019-09-04 21:32:50 -0500

    updates

--- sys/printsegaddress.c
+++ sys/printsegaddress.c
@@ -9,9 +9,9 @@ extern WORD _end;
 void printsegaddress() {
         kprintf("Current: etext[0x%08x]=0x%08x, edata[0x%08x]=0x%08x, ebss[0x%08x]=0x%08x\n", &etext, etext, &edata, edata, &_end, _end);
         
-        char* tmp_etext = &etext - 1;
+        int* tmp_etext = &etext - 1;
         char* tmp_edata = &edata - 1;
-        char* tmp_end = &_end - 1;
+        int* tmp_end = &_end - 1;
         kprintf("Preceding: etext[0x%08x]=0x%08x, edata[0x%08x]=0x%08x, ebss[0x%08x]=0x%08x\n", tmp_etext, *tmp_etext, tmp_edata, *tmp_edata, tmp_end, *tmp_end);
 
         tmp_etext = &etext + 1;
