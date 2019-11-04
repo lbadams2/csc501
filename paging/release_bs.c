bc49b36f0939135382400aecb0df33583bf018d2
Liam Adams <liam_adams@mckinsey.com>
2019-11-04 17:19:57 -0600

    updates

--- paging/release_bs.c
+++ paging/release_bs.c
@@ -9,10 +9,9 @@ SYSCALL release_bs(bsd_t bs_id) {
 
   /* release the backing store with ID bs_id */
   bs_map_t *bs = &bsm_tab[bs_id];
-  int pid = getpid();
-  bs->bs_status[pid] = BSM_UNMAPPED;
-  bs->bs_npages[pid] = 0;
-  bs->bs_pid[pid] = 0;
+  bs->bs_status = BSM_UNMAPPED;
+  bs->bs_npages = 0;
+  bs->bs_pid = 0;
   return OK;
 
 }
