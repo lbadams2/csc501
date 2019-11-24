905ff395e3c01cbd7c6df06b6a37f1513891a53d
Liam Adams <liam_adams@mckinsey.com>
2019-11-23 22:44:39 -0600

    first commit

---
+++ disk.h
@@ -0,0 +1,38 @@
+#ifndef _DISK_H_
+#define _DISK_H_
+
+#define N_DBLOCKS 10 
+#define N_IBLOCKS 4 
+#define BBLOCK_SIZE 128
+#define INODE_START 256
+
+typedef struct {
+    int blocksize; /* size of blocks in bytes */
+    int inode_offset; /* offset of inode region in blocks */
+    int data_offset; /* data region offset in blocks */
+    int swap_offset; /* swap region offset in blocks */
+    int free_inode; /* head of free inode list */
+    int free_block; /* head of free block list */
+} superblock;
+
+typedef struct {  
+      int next_inode; /* list for free inodes */  
+      int protect;        /*  protection field */ 
+      int nlink;  /* Number of links to this file */ 
+      int size;  /* Number of bytes in file */   
+      int uid;   /* Owner's user ID */  
+      int gid;   /* Owner's group ID */  
+      int ctime;  /* Time field */  
+      int mtime;  /* Time field */  
+      int atime;  /* Time field */  
+      int dblocks[N_DBLOCKS];   /* Pointers to data blocks */  
+      int iblocks[N_IBLOCKS];   /* Pointers to indirect blocks */  
+      int i2block;     /* Pointer to doubly indirect block */  
+      int i3block;     /* Pointer to triply indirect block */  
+} inode;
+
+int *buffer;
+superblock *sb;
+extern int blocksize_int;
+
+#endif
--- main.c
+++ main.c
@@ -1,36 +1,72 @@
-/* user.c - main */
-
-#include <conf.h>
-#include <kernel.h>
-#include <proc.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <assert.h> 
+#include "disk.h"
 
-/*------------------------------------------------------------------------
- *  main  --  user main program
- *------------------------------------------------------------------------
- */
+int blocksize_int;
 
-//extern long zfunction(long param);
-extern void syscallsummary_start();
-extern void syscallsummary_stop();
-extern void printsyscallsummary();
-
-long mask_shift(long num) {
-    u_int32_t mask = 0xfffc01ff;
-    long result = num & mask;
-    result = result >> 8;
-    return result;
+void read_disk(char *file_name) {
+    FILE *fp;
+    size_t bytes;
+    fp = fopen(file_name,"r");
+    int fd = fileno(fp);
+    struct stat finfo;
+    fstat(fd, &finfo);
+    buffer = malloc(finfo.st_size);
+    bytes = fread(buffer,finfo.st_size,1,fp);
 }
 
-int main()
-{
-        kprintf("\n\nHello World, Xinu lives\n\n");
-        //zfunction(156);
-        //printtos();
-        //printprocstks(0);
-        syscallsummary_start();
-        getpid();
-        syscallsummary_stop();
-        printsyscallsummary();
-        return 0;
+void set_sb() {
+    sb = (superblock*) &(buffer[BBLOCK_SIZE]);
+    unsigned long tmp = (unsigned long)sb;
+    assert(tmp % 4 == 0);
+    assert(sizeof(superblock) == 24);
+    assert(sizeof(inode) == 100);
+    sb->blocksize = buffer[BBLOCK_SIZE];
+    blocksize_int = sb->blocksize / 4;
+    printf("block size is %d\n", sb->blocksize);
+    sb->inode_offset = buffer[BBLOCK_SIZE + 1];
+    printf("inode offset is %d\n", sb->inode_offset);
+    sb->data_offset = buffer[BBLOCK_SIZE + 2];
+    printf("data offset is %d\n", sb->data_offset);
+    sb->swap_offset = buffer[BBLOCK_SIZE + 3];
+    printf("swap offset is %d\n", sb->swap_offset);
+    sb->free_inode = buffer[BBLOCK_SIZE + 4];
+    printf("free inode is %d\n", sb->free_inode);
+    sb->free_block = buffer[BBLOCK_SIZE + 5];
+    printf("free block is %d\n", sb->free_block);
+}
+
+void read_inodes() {
+    int in_offset = INODE_START + sb->inode_offset * blocksize_int;
+    inode *in =(inode *) &(buffer[in_offset]);
+    
+    int i, j;
+    
+
+    int data_offset = INODE_START + sb->data_offset * blocksize_int;
+    for(i = 0; i < data_offset; i+=25) {
+        in = in + i;
+        printf("printing inode %d\n", i/25);
+        printf("next inode is %d\n", in->next_inode);
+        for(j = 0; j < N_DBLOCKS; j++) {
+            printf("pointer to dblock %d is %d\n", j, in->dblocks[j]);
+        }
+        for(j = 0; j < N_IBLOCKS; j++) {
+            printf("pointer to iblock %d is %d\n", j, in->iblocks[j]);
+        }
+        printf("\n\n\n");
+    }
+}
+
+
+int main(int argc, char **argv) {
+    char *file_name = argv[1];
+    read_disk(file_name);
+    set_sb();
+    read_inodes();
+    return 0;
 }
