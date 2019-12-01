c22b02dc2932e26039a7508b7a8dc44dff0dd1bc
Liam Adams <liam_adams@mckinsey.com>
2019-12-01 11:14:17 -0600

    updates

--- sys/main.c
+++ sys/main.c
@@ -203,9 +203,9 @@ int main( )
          * The provided results do not guarantee your correctness.
          * You need to read the PA2 instruction carefully.
          */
-        test_varargs(3, 4, 5, 6);
-	//test1();
-	//test2();
+        //test_varargs(3, 4, 5, 6);
+	test1();
+	test2();
 	//test3();
 
         /* The hook to shutdown QEMU for process-like execution of XINU.
